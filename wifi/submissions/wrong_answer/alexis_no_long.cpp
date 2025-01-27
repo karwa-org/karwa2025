ba860be9919bc7d86281516de54d82c4a9460297
AlexisEnglebert <englebert.alexis@gmail.com>
2025-01-27 16:30:23 +0100

    wifi: Added solution & removed partial scoring files

---
+++ wifi/submissions/wrong_answer/alexis_no_long.cpp
@@ -0,0 +1,41 @@
+/*
+ * Solution en O(q * log(n) + n log(n)) with c++ upper_bound
+ * @EXPECTED_RESULTS@: WRONG_ANSWER
+ */
+
+#include <stdio.h>
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+
+signed main() {
+    int n, q; cin >> n >> q;
+
+    set<int> a;
+    for (int i = 0; i < n; i++){
+        int v; cin >> v;
+        a.insert(v);
+    }
+
+    for (int i = 0; i < q; i++) {
+        int val; cin >> val;
+        auto it = a.upper_bound(val);
+        if(it == a.end()){
+            cout << *prev(it) << endl;
+        } else {
+            if(it == a.begin()) {
+                cout << *it << endl;
+                continue;
+            }
+            int first = abs(val - *it);
+            int second = abs(val - *prev(it));
+            if(first <= second) {
+                cout << *it << endl;
+            }else {
+                cout << *prev(it) << endl;
+            }
+        }
+    }
+}
