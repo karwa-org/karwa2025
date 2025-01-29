942bb059dd12a2c6551aa97710f2947705a4b91b
AlexisEnglebert <englebert.alexis@gmail.com>
2025-01-29 13:06:27 +0100

    wifi: fixed input validator

--- wifi/input_validators/input_validator/input_validator.cpp
+++ wifi/input_validators/input_validator/input_validator.cpp
@@ -1,5 +1,6 @@
 #include "validation.h"
 #include <map>
+
 int main(int argc, char *argv[]) {
 
     InputValidator v(argc, argv);
@@ -8,14 +9,15 @@ int main(int argc, char *argv[]) {
     long long q = v.read_integer("q", 1, 2*(long long)1e5);
     v.newline();
 
-    map<int, bool> cnt;
+    std::map<int, bool> cnt;
 
     v.read_integers("a", n, 0, (long long)1e18, Unique); // Already check for newline at the end
 
     for(int i = 0; i < q; i++) {
         long long val = v.read_integer("ai", 0, (long long)1e18);
         if (cnt[val]) {
-            WA("Expected antennes to have a unique position ");
+            std::cerr << "Expected antennes to have a unique position " << std::endl;
+            return 43;
         }
         v.newline();
     }
