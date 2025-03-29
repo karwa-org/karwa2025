2b00429dc9ddb04a862734e8d5962f6f34c25f73
chrisgdt <chgrandmont@gmail.com>
2025-03-29 21:53:12 +0100

    Fix symlink, typos, warnings, etc

--- artefact/input_validators/input_validator/input_validator.cpp
+++ artefact/input_validators/input_validator/input_validator.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
     InputValidator v(argc, argv);
-    int n = v.read_integer("length", 1, 3000);
+    int n = v.read_integer("nbr_of_artifacts", 1, 300);
     v.newline();
     v.read_integers("elements", n, 1, (int long)1e9);
     return 0;
