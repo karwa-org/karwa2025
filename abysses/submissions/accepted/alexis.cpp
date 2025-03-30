544f83e499f032a9c343d51db547b26aa31ab843
AlexisEnglebert <englebert.alexis@gmail.com>
2025-03-31 00:32:23 +0200

    Abysses: fixed problem

--- abysses/submissions/accepted/alexis.cpp
+++ abysses/submissions/accepted/alexis.cpp
@@ -17,7 +17,7 @@
 using namespace std;
 
 const int INF = numeric_limits<int>::max()/2;
-#define EPSILON 1e-4
+#define EPSILON 1e-3
 
 typedef struct {
     int idx;
@@ -32,9 +32,10 @@ karwa_t rotate_point(karwa_t pt, int deg) {
     double s_theta = sin(rad);
 
     double new_x = c_theta * pt.x - s_theta * pt.y;
-    double new_y = s_theta * pt.y - c_theta * pt.x;
+    double new_y = s_theta* pt.x + c_theta * pt.y;
 
     int new_deg = (pt.deg + deg)%360;
+
     return {pt.idx, new_x, new_y, new_deg};
 }
 
@@ -56,52 +57,45 @@ void solve() {
 
     // Rotate the plane at 360
     for(int rot = 0; rot < 360; rot++) {
+       // if (rot == 180 || rot == 90 || rot == 270) continue; //balek
         //perform the rotation of the plane 
         vector<karwa_t> new_points;
-        for(auto& point : karwa) {
+        for(auto point : karwa) {
             new_points.push_back(rotate_point(point, rot));
         }
-        
         // Sort by y and if same y (then collinear sort them by least x)
         sort(new_points.begin(), new_points.end(), [](karwa_t a, karwa_t b) {
-            if((a.y -b.y) < EPSILON){
-                return a.x < b.x;
-            } else {
+            if(fabs(a.y - b.y) > EPSILON){
                 return a.y > b.y;
-            }
+            } 
+            return a.x < b.x;
+
         });
 
-        cerr << "rotation : " << rot <<  " -  ";
-
-        for(auto& p : new_points){
-            cerr << "( " << p.x << " "  << p.y << ")" << ", ";
-        }
-        cerr << endl;
-
         // Now a sweep line and construct the graph
-        for(int j = 0; j < n-1; j++) {
-            if (new_points[j].deg != 0) continue;
-            cerr << "wtf" << endl;
+        
+        for(int j = 0; j < (int)(n-1); j++) {
+            if (new_points[j].deg > EPSILON) continue;
+
             if((new_points[j].y - new_points[j+1].y) < EPSILON) {
                 adj[new_points[j].idx].push_back(new_points[j+1].idx);
-                adj[new_points[j+1].idx].push_back(new_points[j].idx);
             }
         }
     }
 
-    for(int i = 0; i < n; i++) {
-        cerr << "adj["<<i<<"] : ";
-        for(auto& k : adj[i]) cerr << k << " ";
-        cerr << endl;
-    }
 
     // Now a bfs to find the size of the component
     queue<int> q;
     q.push(fish);
     vector<bool> visited (n, false);
     int ans = 0;
+   
+
     while(!q.empty()){
-        int current = q.front(); q.pop();
+        int current = q.front(); q.pop();        
+        if(visited[current] == true) {            
+            continue;
+        }
         ans ++;
         visited[current] = true;
         for(auto& k : adj[current]) {
