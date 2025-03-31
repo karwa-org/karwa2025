4ccfd176a98ce12d771363a8ebe8cd812d61fc51
chrisgdt <chgrandmont@gmail.com>
2025-03-31 13:56:12 +0200

    Ajusting abysse, fix images, statements

--- abysses/input_validators/input_validator/input_validator.cpp
+++ abysses/input_validators/input_validator/input_validator.cpp
@@ -1,27 +1,30 @@
 #include "validation.h"
 
-// Check the grammar of the input files.
-// You should also check properties of the input.
-// E.g., check that a graph is connected.
-
 int main(int argc, char *argv[]) {
     InputValidator v(argc, argv);
-    int n = v.read_integer("n", 0, 100000);
-    v.space();
-    float f = v.read_float("f", 0, 100000);
+    int n = v.read_integer("nbr_of_fish", 0, 1000);
     v.newline();
-    return 0;
 
-    // Other useful commands:
-    // read_{float,integer}[s] takes an optional tag:
-    // Unique, Increasing, Decreasing, StrictlyIncreasing, StrictlyDecreasing
-    v.read_integers("v", /*count=*/10, 0, 1000000, Unique);
-    v.test_string("ACCEPTED"); // only succeeds when it reads the given string.
-    v.read_string("s", 4, 5);     // only succeeds when it reads a string with length in inclusive range.
-    bool b = v.peek('x'); // test the next character.
-    v.WA("The input is not valid."); // Print error and exit with code 43.
-    v.check(false, "WA on false");
+    std::set<std::pair<long long, long long>> fish_positions;
+    const long double precision = 1e-5;
+    const long double scale = 1e5;
 
-    // In its destructor, v automatically exits with code 42 here.
-    // TODO: Remove this comment, and summarize your input validator.
+    for (int i = 0; i < n; i++) {
+        long double x = v.read_float("pos_x", 0, (long double)1e7);
+        v.space();
+        long double y = v.read_float("pos_y", 0, (long double)1e7);
+        v.space();
+        v.read_integer("angle", 0, 359);
+        v.newline();
+
+        long long x_scaled = std::llround(x * scale);
+        long long y_scaled = std::llround(y * scale);
+
+        if (!fish_positions.insert({x_scaled, y_scaled}).second) {
+            v.WA("Two fish are too close to each other.");
+        }
+    }
+
+    v.read_integer("target_fish", 0, n-1);
+    v.newline();
 }
